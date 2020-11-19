5349979d8705f8d02349ad3c3ad8d741e5cde08e
Mason Howe <mason.a.howe@gmail.com>
2020-11-19 13:32:22 -0700

    Add True False

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ var questions = [{
     {
         Type: "MultipleChoice",
         Question: "How would one declare a function in Javascript",
-        Correct: "function name (){code}",
+        Correct: "function name(){code}",
         Incorrect: "function.name.{code}",
         Incorrect: "function:name{code}"
     },
     {
-        Type: "MultipleChoice",
+        Type: "MultipleSelect",
         Question: "Selct all the primitive data types",
         Correct: "String",
         Correct: "Number",
@@ -25,6 +25,7 @@ var questions = [{
 
 var time = 2000
 let questionIndex = 0
+body = document.body
 
 
 document.getElementById("start-btn").addEventListener("click", function () {
@@ -37,7 +38,7 @@ document.getElementById("start-btn").addEventListener("click", function () {
 
 })
 
-document.getElementById("next-btn").addEventListener("click", function() {
+document.getElementById("next-btn").addEventListener("click", function () {
     console.log(questionIndex)
     displayQuestion(questionIndex)
     questionIndex = questionIndex + 1
@@ -73,9 +74,84 @@ function timer() {
 
 function displayQuestion(index) {
 
+    // add question test
     let question = document.getElementById("question-text").innerText = questions[index]["Question"]
     document.getElementById("question-text-wrapper").classList.remove("hide")
     //console.log(question)
 
     // add question items
+    questionType = questions[index]["Type"]
+    console.log(questionType)
+
+    if (questionType == "True/False") {
+        trueFalse(questions[index])
+    } else if (questionType == "MultipleChoice") {
+        multipleChoice(question[index])
+    } else if (questionType == "MultipleSelect") {
+        MultiplesSelect(question[index])
+    }
+
+
+
+}
+
+function trueFalse(question) {
+    console.log("true false function")
+    let questionWrapper = document.getElementById("quiz-content-questions-wrapper")
+    // add question holder that we can remove from the parent during the next question
+    let questionHolder = document.createElement("div")
+    questionWrapper.appendChild(questionHolder)
+
+    // add true false question
+    // add true   
+    let questionTrue = document.createElement("input")
+    questionTrue.setAttribute("type", "radio")
+    questionTrue.setAttribute("name", "TF")
+    questionTrue.setAttribute("id", "True")
+    questionTrue.setAttribute("value", "true")
+    questionHolder.appendChild(questionTrue)
+    trueLabel = document.createElement("label")
+    trueLabel.setAttribute("for", "True")
+    questionHolder.append(trueLabel)
+    trueLabel.innerText = "True"
+
+    //add false
+    let questionFalse = document.createElement("input")
+    questionFalse.setAttribute("type", "radio")
+    questionFalse.setAttribute("name", "TF")
+    questionFalse.setAttribute("id", "False")
+    questionFalse.setAttribute("value", "false")
+    questionHolder.appendChild(questionFalse)
+    trueLabel = document.createElement("label")
+    trueLabel.setAttribute("for", "False")
+    questionHolder.append(trueLabel)
+    trueLabel.innerText = "False"
+
+    // get status on change. 
+    var radios = document.querySelectorAll('[name="TF"]');
+
+    function changeHandler(event) {
+        if (this.value === 'true') {
+            console.log('value', 'true');
+        } else if (this.value === 'false') {
+            console.log('value', 'false');
+        }
+    }
+
+    Array.prototype.forEach.call(radios, function (radio) {
+        radio.addEventListener('change', changeHandler);
+    });
+
+
+
+
+}
+
+function multipleChoice(question) {
+    console.log("multiple Choice Function")
+
+}
+
+function MultiplesSelect(question) {
+    console.log("multiple select function")
 }
